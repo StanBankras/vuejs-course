578ef6e1e54e00877339699ec84be6dadf6e87a1
Stan <Stanbankras@gmail.com>
2020-02-18 10:16:51 +0100

    Setup a local development server with VueJS using NodeJS

---
+++ vue-development-server/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Stan'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

549508c4830b4bf4fdcea37c2b17f3e74f5be309
Stan <Stanbankras@gmail.com>
2020-02-18 14:55:59 +0100

    Handling forms with Vue part 1

--- vue-development-server/src/App.vue
+++ vue-development-server/src/App.vue
@@ -1,78 +1,98 @@
 <template>
     <div>
-        <app-header></app-header>
+        <app-header/>
         <div class="container">
-            <compUserProfile 
-                :userName="name"
-                :userLastName="lastname"
-                :userAge="age"
-                :userParents="parents"
-                @updateName="name = $event"
-                :updateLastName="updateLastName"
-            />
-            <compFriends/>
-            <compHabilities>
-                <ul slot="main_hab">
-                    <li v-for="(hab, index) in habilities" :key="index">{{ hab }}</li>
-                </ul>
-                <div slot="notes">
-                    <p>The user also knows PHP</p>
-                </div>
-                <a href="#">Link</a>
-                <div :slot="slotName">
-                    Other content
-                </div>
-            </compHabilities>
+           <form>
+               <div class="row">
+                   <div class="col-xs-12">
+                        <h1>Contact us</h1>
+                        <hr>
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input 
+                                type="text" 
+                                id="name"
+                                class="form-control"
+                                v-model.lazy="formdata.name"
+                            >
+                            {{ formdata.name }}
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname">Lastname</label>
+                            <input 
+                                type="text" 
+                                id="lastname"
+                                class="form-control"
+                                v-model="formdata.lastname"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">Subject</label>
+                            <input 
+                                type="text" 
+                                id="subject"
+                                class="form-control"
+                                v-model="formdata.subject"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea
+                                class="form-control"
+                                rows="3"
+                                id="message"
+                                v-model="formdata.message"
+                            ></textarea>                            
+                        </div>
+                        <button class="btn btn-primary" @click.prevent="submitForm">
+                            Submit
+                        </button>
+                   </div>
+               </div>
+           </form>
         </div>
-        <compFooter></compFooter>
     </div>
 </template>
 
 <script>
-import compFooter from './components/header_footer/Footer.vue'
-import compUserProfile from './components/user/Profile.vue'
-import compFriends from './components/user/Friends.vue'
-import compHabilities from './components/user/Habilities.vue'
-
-export default {
-    data() {
-        return {
-            name: 'Stan',
-            lastname: 'Jones',
-            age: 25,
-            parents: {
-                mother: "Martha",
-                father: "Mario"
-            },
-            habilities: ['JS', 'CSS', 'HTML'],
-            slotName: ''
+    export default {
+        data(){
+            return {
+                formdata:{
+                    name:'',
+                    lastname:'',
+                    subject:'',
+                    message:'Some default content',
+                    extras:[],
+                    gender:'alien',
+                    country:'India'
+                },
+                countries:[
+                    'EEUU',
+                    'India',
+                    'UK',
+                    'Russia'
+                ],
+            }
+        },
+        methods:{
+            submitForm(){
+                console.log(this.formdata);
+                console.log(JSON.stringify(this.formdata))
+            }
         }
-    },
-    components: {
-        compFooter,
-        compUserProfile,
-        compFriends,
-        compHabilities
-    },
-    methods: {
-        updateLastName(value) {
-            this.lastname = value;
-        }
-    },
-    created() {
-        setTimeout(() => {
-            this.slotName = 'other';
-        }, 3000)
-    },
-}
+    }
 </script>
+
 
 <style>
     body {
-        padding: 0;
-        margin: 0;
+        padding:0;
+        margin:0;
         font-family: 'Roboto', sans-serif;
     }
+
     .container {
         min-height: 84vh;
         box-sizing: border-box;

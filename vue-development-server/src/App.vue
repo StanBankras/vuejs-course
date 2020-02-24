d1a9d774a34d9c2e3824fb1a329ef22b0cdc3760
Stan <Stanbankras@gmail.com>
2020-02-24 13:57:25 +0100

    Transition groups to animate for example adding items to a list

--- vue-development-server/src/App.vue
+++ vue-development-server/src/App.vue
@@ -2,41 +2,64 @@
     <div>
         <app-header/>
         <div class="container">
+            <input v-model="name">
+            <button class="btn btn-primary" @click="addOne">Add one</button>
 
-            <transition name="slideup">
-                <div class="p-3 mb-2 bg-success text-white" v-if="display">Hello 2</div>            
-            </transition>
+            <ul class="list-group">
+                <transition-group name="appear">
+                    <li
+                        class="list-group-item"
+                        v-for="(item,index) in list"
+                        :key="item"
+                        @click="removeItem(index)"
+                    >
+                        {{ item }}
+                    </li>
+                </transition-group>
+                
+            </ul>
 
-            <transition name="appear">
-                <div class="p-3 mb-2 bg-success text-white" v-if="display">Hello</div>            
-            </transition>
-
-            <button @click="display = !display" class="btn btn-primary">Toggle animation</button>
 
         </div>
+        <app-footer/>
     </div>
 </template>
 
 <script>
+
     export default {
         data(){
             return {
-                display: false
+                list:['Francis','Ron','James'],
+                velocity:false,
+                hook:false,
+                display: false,
+                status: false,
+                custom: true,
+                name:''
             }
+        },
+        methods:{
+            addOne(){
+                if(!this.list.includes(this.name)){
+                    this.list.unshift(this.name)
+                }
+            },
+            removeItem(index){
+                this.list.splice(index,1)
+            },           
         }
     }
 </script>
 
-
 <style>
-
     .appear-enter {
         transform: translateX(30px);
         opacity: 0;
     }
 
     .appear-enter-active {
-        transition: all .3s ease;
+        transition: all 0.3s ease;
     }
 
     .appear-leave {
@@ -46,15 +69,20 @@
     .appear-leave-active {
         transform: translateX(30px);
         opacity: 0;
-        transition: all .3s ease;
+        transition: all 0.3s ease;
+        position: absolute;
+    }
+    .appear-move {
+        transition: transform 0.3s;
     }
 
     .slideup-enter {
-
+        opacity: 0;
     }
 
     .slideup-enter-active {
-        animation: slideUp 1s ease-out;
+        transition: opacity 1s;
+        animation:  slideUp 1s ease-out;
     }
 
     .slideup-leave {
@@ -62,27 +90,29 @@
     }
 
     .slideup-leave-active {
-        animation: slideDown 1s ease-out;
-
+        opacity: 0;
+        transition: opacity 1s;
+        animation:  slideDown 1s ease-out;
     }
+
 
     @keyframes slideUp {
         from {
-            transform: translateY(20px);
+            transform: translateY(20px)
         }
         to {
-            transform: translateY(0);
+            transform: translateY(0)
+        }
+    }
+     @keyframes slideDown {
+        from {
+            transform: translateY(0)
+        }
+        to {
+            transform: translateY(20px)
         }
     }
 
-    @keyframes slideDown {
-        from {
-            transform: translateY(0);
-        }
-        to {
-            transform: translateY(20px);            
-        }
-    }
 
     body {
         padding:0;

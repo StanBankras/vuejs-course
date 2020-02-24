c403b36aadfdb09b7f7966ee59c6bbe5ab7ddd38
Stan <Stanbankras@gmail.com>
2020-02-24 10:33:37 +0100

    Forms: Radio buttons

--- vue-development-server/src/App.vue
+++ vue-development-server/src/App.vue
@@ -61,9 +61,28 @@
                             id="promotions"
                             value="promotions"
                             v-model="formdata.extras"
-                            >     
-                            <br>
-                            {{ formdata.extras }}            
+                            >            
+                        </div>
+
+                        <div class="form-group">
+                            <label for="human">   
+                                <input 
+                                    type="radio"
+                                    id="human"
+                                    value="human"
+                                    v-model="formdata.gender"
+                                > 
+                                Human    
+                            </label>    
+                            <label for="alien">   
+                                <input 
+                                    type="radio"
+                                    id="alien"
+                                    value="alien"
+                                    v-model="formdata.gender"
+                                > 
+                                Alien    
+                            </label>          
                         </div>
 
                         <button class="btn btn-primary" @click.prevent="submitForm">
@@ -88,7 +107,8 @@
                     extras:[],
                     gender:'alien',
                     country:'India',
-                    extras: []
+                    extras: [],
+                    gender: 'alien'
                 },
                 countries:[
                     'EEUU',

493e7284729d0ad3ada95a53af032c2c6fa1359e
Stan <Stanbankras@gmail.com>
2020-02-27 10:28:38 +0100

    Using json-server package and vue-resource to GET data from a fake database (part 1)

--- vue-development-server/src/App.vue
+++ vue-development-server/src/App.vue
@@ -2,116 +2,38 @@
     <div>
         <app-header/>
         <div class="container">
-            <input v-model="name">
-            <button class="btn btn-primary" @click="addOne">Add one</button>
-
-            <ul class="list-group">
-                <transition-group name="appear">
-                    <li
-                        class="list-group-item"
-                        v-for="(item,index) in list"
-                        :key="item"
-                        @click="removeItem(index)"
-                    >
-                        {{ item }}
-                    </li>
-                </transition-group>
-                
-            </ul>
-
-
+            <div>
+                <button @click="active = 'compUsers'">Users</button>
+                <button @click="active = 'compAddUsers'">Add users</button>
+            </div>
+            <hr>
+            
+            <div>
+                <component :is="this.active"></component>
+            </div>
         </div>
         <app-footer/>
     </div>
 </template>
 
 <script>
+    import compUsers from './Components/Users.vue';
+    import compAddUsers from './Components/AddUsers.vue';
 
     export default {
         data(){
-            return {
-                list:['Francis','Ron','James'],
-                velocity:false,
-                hook:false,
-                display: false,
-                status: false,
-                custom: true,
-                name:''
+            return{
+                active:'compUsers'
             }
         },
-        methods:{
-            addOne(){
-                if(!this.list.includes(this.name)){
-                    this.list.unshift(this.name)
-                }
-            },
-            removeItem(index){
-                this.list.splice(index,1)
-            },           
+        components:{
+            compUsers,
+            compAddUsers
         }
     }
 </script>
 
 <style>
-    .appear-enter {
-        transform: translateX(30px);
-        opacity: 0;
-    }
-
-    .appear-enter-active {
-        transition: all 0.3s ease;
-    }
-
-    .appear-leave {
-
-    }
-
-    .appear-leave-active {
-        transform: translateX(30px);
-        opacity: 0;
-        transition: all 0.3s ease;
-        position: absolute;
-    }
-    .appear-move {
-        transition: transform 0.3s;
-    }
-
-    .slideup-enter {
-        opacity: 0;
-    }
-
-    .slideup-enter-active {
-        transition: opacity 1s;
-        animation:  slideUp 1s ease-out;
-    }
-
-    .slideup-leave {
-
-    }
-
-    .slideup-leave-active {
-        opacity: 0;
-        transition: opacity 1s;
-        animation:  slideDown 1s ease-out;
-    }
-
-
-    @keyframes slideUp {
-        from {
-            transform: translateY(20px)
-        }
-        to {
-            transform: translateY(0)
-        }
-    }
-     @keyframes slideDown {
-        from {
-            transform: translateY(0)
-        }
-        to {
-            transform: translateY(20px)
-        }
-    }
 
 
     body {
--- vue-development-server/src/main.js
+++ vue-development-server/src/main.js
@@ -1,43 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 import Header from './components/header_footer/Header.vue'
 
 Vue.component('app-header', Header);
 
-Vue.directive('awesome', { 
-    bind(el, binding, vnode) {
-
-        el.innerHTML = binding.value;
-
-        // if (binding.arg == 'red') {
-        //     el.style.color = 'red';
-        // } else {
-        //     el.style.color = 'blue';
-        // }
-
-        el.style.color = binding.modifiers.red ? 'red' : 'blue';
-        el.style.fontSize = binding.modifiers.big ? '30px' : '12px';
-        // happens before the element has been inserted in the dom
-        console.log(el.parentNode);
-    },
-    inserted(el, binding, vnode) {
-        // runs after the element has been inserted in the dom
-        console.log(el.parentNode);
-    },
-    update(el, binding, vnode, oldVnode) {
-        console.log('update');
-    },
-    componentUpdated() {
-        console.log('update too');
-    },
-    unbind(el, binding, vnode) {
-        console.log('unbind');
-    }
-});
-
-
-export const bus = new Vue();
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
